43b283cef86d02ab890f9db30aea32bfdd227352
Maestro <volodimirkucak996@gmail.com>
2026-09-16 11:20:22 +0300

    lab01: solution CrossApp, Cli, Склад

---
+++ README.md
@@ -0,0 +1,19 @@
+# CrossApp
+Наскрізний проєкт з крос-платформного програмування.
+Предметна область: Склад. Сутності: Product, StockBatch, Warehouse, Movement. 
+Призначення: облік залишків товарів по партіях.
+## Запуск
+dotnet build
+dotnet run --project src/Cli
+## Середовище
+.NET SDK 10.0, Mac OS X arm64 
+
+Додаткові завдання 
+
+## Розміри self-contained збірок:
+osx-arm64: 83M
+win-x64: 77M
+
+2. 
+    ### Запуск для виводу в json-форматі 
+    dotnet run --project src/Cli -- --json 
---
+++ src/Cli/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json;
+
+if (args.Length > 0 && args[0] == "--json")
+{
+    var appInfo = new Dictionary<string, object?>
+    {
+        ["title"] = "CrossApp – практикум з крос-платформного програмування",
+        ["student"] = "Студент: Кучак Володимир, ФЕІ-33",
+        ["chapter"] = new string('-', 52),
+        ["ОС (OSDescription)"] = RuntimeInformation.OSDescription,
+        ["ОС (Environment)"] = Environment.OSVersion,
+        ["Архітектура процесу"] = RuntimeInformation.ProcessArchitecture,
+        ["Версія.NET(CLR)"] = Environment.Version,
+        ["Runtime"] = RuntimeInformation.FrameworkDescription,
+        ["Каталог застосунку"] = AppContext.BaseDirectory,
+        ["Поточний каталог"] = Environment.CurrentDirectory,
+        ["chapter2"] = new string('-', 52),
+        ["Предметна область"] = "Склад (товари, партії, залишки, переміщення)"
+    };
+
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+    };
+
+    string jsonOut = JsonSerializer.Serialize(appInfo, options);
+    Console.WriteLine(jsonOut);
+}
+else
+{
+    Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+    Console.WriteLine("Студент: Кучак Володимир, ФЕІ-33");
+    Console.WriteLine(new string('-', 52));
+    Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+    Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+    Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+    Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+    Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+    Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+    Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+    Console.WriteLine(new string('-', 52));
+    Console.WriteLine("Предметна область: Склад (товари, партії, залишки, переміщення)");
+}
